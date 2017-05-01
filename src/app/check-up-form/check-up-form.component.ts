d7fd6cb8375f09075872c29e6c402ecec4b21bae
Sunnycureyo <Sunny@cureyo.com>
2017-05-01 18:28:08 +0530

    checkup Form uncomplete model implementation

--- src/app/check-up-form/check-up-form.component.ts
+++ src/app/check-up-form/check-up-form.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import * as moment from 'moment';
+import { Component, OnInit, Input } from '@angular/core';
+import {FormGroup, FormBuilder, Validators,FormArray} from "@angular/forms";
+import {ActivatedRoute, Router} from "@angular/router";
+import {AuthService} from "../services/firebaseauth.service";
+//import {MedReminder} from "../../../models/medReminder.interface";
+import {FbService} from "../services/facebook.service";
+
  declare var jQuery:any; 
 @Component({
   selector: 'app-check-up-form',
@@ -7,25 +12,68 @@ import * as moment from 'moment';
   styleUrls: ['./check-up-form.component.css']
 })
 export class CheckUpFormComponent implements OnInit {
+   public checkupForm: FormGroup;
+  constructor(
+              private _fb: FormBuilder,
+              private _fs: FbService,
+              private route: ActivatedRoute,
+              private _authService: AuthService,
+              private router: Router
+  ) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() { 
+        this.checkupForm = this._fb.group({
+         first_Name:[,Validators.required],
+         last_Name:[,Validators.required],
+         Email:[,Validators.required],
+         phone:[,Validators.required],
+         DOB:[,Validators.required],
+         visit_Type:[,Validators.required],
+         description:[,Validators.required],
+         conditions: this._fb.array([
+        this.initConditions()
+      ])
+         });
+    
   }
   
-   MyFunction(){
-     jQuery(function MyFunction() {
-              jQuery('input[name="birthdate"]').daterangepicker({
-                  singleDatePicker: true,
-                  showDropdowns: true
-              }, 
-              function(start, end, label) {
-                  var years = moment().diff(start, 'years');
-                  //alert("You are " + years + " years old.");
-              });
-          });
-       
-   }       
+    initConditions(){
+      return this._fb.group({
+       existingCondition:[],
+       since:[,Validators.required],
+       when:[,Validators.required],
+    });
+  }//initLabTest's
+  AddConditions(){
+      console.log("Add conditions called");
+    const control = <FormArray>this.checkupForm.controls['conditions'];
+    control.push(this.initConditions());
+
+  }//addLabTest
+  
+
+  save_checkUpForm =(model)=>{
+         
+        let job = model['value'],
+        labtests = job['conditions'],
+        ctr = 0,
+        flag;
+        
+            let reminders = {
+                    "First_Name":job['first_Name'],
+                    "Last_Name": job['last_Name'],
+                    "Email":job['Email'],
+                    "Phone": job['phone'],
+                    "DOB":job['DOB'],
+                    "Visit_Type":job['visit_Type'],
+                    "description":job['description:'],
+
+                    "conditions": []
+            };
+              console.log("length of conditions array:",reminders.conditions,length);
+
+  }
+   
   
     
           
---
+++ src/app/models/existingCondition.interface.ts
@@ -0,0 +1,11 @@
+export class ExistingConditions{
+  conditions: Condition[];
+}
+
+export class Condition{
+
+   conditions_option: string;
+    since: string;
+    when: string;
+
+}
